6f7c1bdf0c0efad28e0ea866c0a9c8fd4c5827fb
Matthew Hickman <Matthickman28@gmail.com>
2026-03-08 20:57:44 -0400

    Expanded game_data.py with new cars and boss cars

---
+++ game_data.py
@@ -0,0 +1,39 @@
+# Game Data for 1320 A New Era
+
+# Playable Cars
+PLAYABLE_CARS = {
+    '1': {'name': 'Car 1', 'hp': 300, 'torque': 350, 'weight': 3200, 'redline': 7000, 'quarterMile': 12.5, 'trapSpeed': 110},
+    '2': {'name': 'Car 2', 'hp': 320, 'torque': 360, 'weight': 3300, 'redline': 7200, 'quarterMile': 12.3, 'trapSpeed': 115},
+    '3': {'name': 'Car 3', 'hp': 340, 'torque': 375, 'weight': 3400, 'redline': 7400, 'quarterMile': 12.1, 'trapSpeed': 120},
+    '4': {'name': 'Car 4', 'hp': 350, 'torque': 380, 'weight': 3500, 'redline': 7500, 'quarterMile': 11.9, 'trapSpeed': 125},
+    '5': {'name': 'Car 5', 'hp': 360, 'torque': 400, 'weight': 3600, 'redline': 7600, 'quarterMile': 11.7, 'trapSpeed': 130},
+    # Oddball cars
+    '6': {'name': 'Oddball Car 1', 'hp': 250, 'torque': 250, 'weight': 2800, 'redline': 6800, 'quarterMile': 13.8, 'trapSpeed': 105},
+    '7': {'name': 'Oddball Car 2', 'hp': 220, 'torque': 230, 'weight': 2900, 'redline': 6500, 'quarterMile': 14.2, 'trapSpeed': 100},
+    '8': {'name': 'Oddball Car 3', 'hp': 180, 'torque': 200, 'weight': 2700, 'redline': 6000, 'quarterMile': 14.8, 'trapSpeed': 95},
+    '9': {'name': 'Oddball Car 4', 'hp': 350, 'torque': 300, 'weight': 3100, 'redline': 7200, 'quarterMile': 12.6, 'trapSpeed': 112},
+    '10': {'name': 'Oddball Car 5', 'hp': 400, 'torque': 450, 'weight': 4000, 'redline': 7800, 'quarterMile': 11.5, 'trapSpeed': 135},
+    # More playable cars (15 others)
+    '11': {'name': 'Car 6', 'hp': 330, 'torque': 355, 'weight': 3250, 'redline': 7100, 'quarterMile': 12.4, 'trapSpeed': 118},
+    '12': {'name': 'Car 7', 'hp': 280, 'torque': 270, 'weight': 3000, 'redline': 6900, 'quarterMile': 13.0, 'trapSpeed': 108},
+    '13': {'name': 'Car 8', 'hp': 370, 'torque': 410, 'weight': 3580, 'redline': 7600, 'quarterMile': 11.6, 'trapSpeed': 132},
+    '14': {'name': 'Car 9', 'hp': 390, 'torque': 420, 'weight': 3680, 'redline': 7800, 'quarterMile': 11.4, 'trapSpeed': 137},
+    '15': {'name': 'Car 10', 'hp': 310, 'torque': 340, 'weight': 3150, 'redline': 7000, 'quarterMile': 12.7, 'trapSpeed': 111},
+    '16': {'name': 'Car 11', 'hp': 250, 'torque': 280, 'weight': 2950, 'redline': 6750, 'quarterMile': 14.0, 'trapSpeed': 104},
+    '17': {'name': 'Car 12', 'hp': 400, 'torque': 460, 'weight': 4300, 'redline': 7900, 'quarterMile': 11.2, 'trapSpeed': 140},
+    '18': {'name': 'Car 13', 'hp': 450, 'torque': 495, 'weight': 4400, 'redline': 8000, 'quarterMile': 10.9, 'trapSpeed': 145},
+    '19': {'name': 'Car 14', 'hp': 360, 'torque': 380, 'weight': 3600, 'redline': 7700, 'quarterMile': 11.6, 'trapSpeed': 133},
+    '20': {'name': 'Car 15', 'hp': 220, 'torque': 230, 'weight': 2700, 'redline': 6500, 'quarterMile': 14.3, 'trapSpeed': 103},
+    '21': {'name': 'Car 16', 'hp': 330, 'torque': 355, 'weight': 3250, 'redline': 7100, 'quarterMile': 12.4, 'trapSpeed': 118},
+    '22': {'name': 'Car 17', 'hp': 290, 'torque': 300, 'weight': 3100, 'redline': 6700, 'quarterMile': 13.5, 'trapSpeed': 109},
+    '23': {'name': 'Car 18', 'hp': 370, 'torque': 410, 'weight': 3580, 'redline': 7600, 'quarterMile': 11.6, 'trapSpeed': 132},
+    '24': {'name': 'Car 19', 'hp': 390, 'torque': 420, 'weight': 3680, 'redline': 7800, 'quarterMile': 11.4, 'trapSpeed': 137},
+    '25': {'name': 'Car 20', 'hp': 310, 'torque': 340, 'weight': 3150, 'redline': 7000, 'quarterMile': 12.7, 'trapSpeed': 111}
+}
+
+# Unobtainable Boss-Only Cars
+BOSS_CARS = {
+    'phantom': {'name': '1987 Lamborghini Countach', 'hp': 500, 'torque': 450, 'weight': 3200, 'redline': 8500, 'quarterMile': 10.5, 'trapSpeed': 160},
+    'nitto_god': {'name': 'Custom Tube-Frame Dragster', 'hp': 800, 'torque': 600, 'weight': 1500, 'redline': 9000, 'quarterMile': 8.0, 'trapSpeed': 180},
+    'robo_rob': {'name': 'Modified Corvette Z06', 'hp': 600, 'torque': 500, 'weight': 3100, 'redline': 8800, 'quarterMile': 9.0, 'trapSpeed': 170}
+}
